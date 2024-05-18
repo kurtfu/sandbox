130eb5453f64f19cb9bc5ab516b0957c99a4116e
Furkan Kurt <kurtfu@protonmail.com>
2024-05-18 18:36:25 +0300

    build: add linker configuration

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,4 +1,5 @@
 include(Coverage)
+include(Linker)
 include(Sanitizer)
 include(Warnings)
 
@@ -11,6 +12,8 @@ function(setup_executable)
     _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
     _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
     _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+
+    setup_target_link_strategy(${EXECUTABLE_TARGET})
 
     setup_target_warnings(${EXECUTABLE_TARGET})
     setup_target_for_sanitizer(${EXECUTABLE_TARGET})
@@ -25,6 +28,8 @@ function(setup_coverage)
     _setup_executable_sources(${COVERAGE_TARGET} ${COVERAGE_SOURCES})
     _setup_target_includes(${COVERAGE_TARGET} ${COVERAGE_INCLUDES})
     _setup_target_libraries(${COVERAGE_TARGET} ${COVERAGE_LIBRARIES})
+
+    setup_target_link_strategy(${COVERAGE_TARGET})
 
     setup_target_warnings(${COVERAGE_TARGET})
     setup_target_for_sanitizer(${COVERAGE_TARGET})
