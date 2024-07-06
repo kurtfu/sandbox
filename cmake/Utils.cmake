40e44bd72d12323da4e6113582ef4be987a29859
Furkan Kurt <kurtfu@protonmail.com>
2024-07-06 16:20:24 +0300

    build: add static library generation

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -18,8 +18,30 @@ function(setup_executable)
     setup_target_for_sanitizer(${EXECUTABLE_TARGET})
 endfunction()
 
+function(setup_library)
+    set(oneValueArgs TARGET)
+    set(multiValueArgs SOURCES INCLUDES LIBRARIES)
+
+    cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    _setup_library_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
+    _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
+    _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+
+    setup_target_link_strategy(${EXECUTABLE_TARGET})
+
+    setup_target_warnings(${EXECUTABLE_TARGET})
+    setup_target_for_sanitizer(${EXECUTABLE_TARGET})
+endfunction()
+
 macro(_setup_executable_sources target)
     add_executable(${target}
+        ${ARGN}
+    )
+endmacro()
+
+macro(_setup_library_sources target)
+    add_library(${target}
         ${ARGN}
     )
 endmacro()
