6d0b5667f40e4dfb2402655fe86e907de6c3f0f4
Furkan Kurt <kurtfu@protonmail.com>
2024-07-07 21:58:31 +0300

    refactor: handle compiler warnings for benchmarks

--- benchmark/dummy.cpp
+++ benchmark/dummy.cpp
@@ -3,15 +3,30 @@
 
 #include <benchmark/benchmark.h>
 
+/// \cond
 #include <memory>  // IWYU pragma: keep
+
+/// \endcond
+
+/*****************************************************************************/
+/*** MACRO DEFINITIONS *******************************************************/
+
+#if __cplusplus < 201703L
+    #define BENCHMARK_MAYBE_UNUSED
+#else
+    #define BENCHMARK_MAYBE_UNUSED [[maybe_unused]]
+#endif  // __cplusplus
 
 /*****************************************************************************/
 /*** BENCHMARKS **************************************************************/
 
-static void bm_dummy(benchmark::State& state)
+namespace
 {
-    for (auto _ : state)
-    {}
-}
+    void dummy(benchmark::State& state)
+    {
+        for (BENCHMARK_MAYBE_UNUSED const auto& _ : state)
+        {}
+    }
+}  // namespace
 
-BENCHMARK(bm_dummy);
+BENCHMARK(dummy);
