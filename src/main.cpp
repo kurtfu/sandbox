e57474baf3ee0f1a7773964c03e1704f3e130d90
Furkan Kurt <kurtfu@protonmail.com>
2024-07-08 22:46:58 +0300

    refactor: simplify main application

--- src/main.cpp
+++ src/main.cpp
@@ -2,63 +2,15 @@
 /*** HEADER INCLUDES *********************************************************/
 
 /// \cond
-#include <csignal>
-#include <exception>
 #include <iostream>
-#include <memory>
-#include <tuple>
 
 /// \endcond
-
-/*****************************************************************************/
-/*** DATA TYPES **************************************************************/
-
-class Service
-{
-public:
-    void start()
-    {
-        m_active = true;
-
-        while (m_active)
-        {}
-    }
-
-    void stop() noexcept
-    {
-        m_active = false;
-    }
-
-private:
-    bool m_active = false;
-};
 
 /*****************************************************************************/
 /*** MAIN APPLICATION ********************************************************/
 
 int main()
 {
-    static std::unique_ptr<Service> service = nullptr;
-
-    try
-    {
-        service = std::make_unique<Service>();
-
-        auto signal_handler = [](int)
-        {
-            std::cout << "SIGINT received!\n";
-            service->stop();
-        };
-
-        std::ignore = std::signal(SIGINT, signal_handler);
-        service->start();
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-        service->stop();
-    }
-
-    std::cout << "goodbye, world\n";
+    std::cout << "hello, world\n";
     return 0;
 }
