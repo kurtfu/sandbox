02879fcf5812071d38d2ca3fdbd3b3448082c274
Furkan Kurt <kurtfu@protonmail.com>
2024-07-16 20:44:03 +0300

    build: change target dependency option

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -4,13 +4,13 @@ include(Warnings)
 
 function(setup_executable)
     set(oneValueArgs TARGET)
-    set(multiValueArgs SOURCES INCLUDES LIBRARIES)
+    set(multiValueArgs SOURCES INCLUDES DEPENDENCIES)
 
     cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
     _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
-    _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+    _setup_target_dependencies(${EXECUTABLE_TARGET} ${EXECUTABLE_DEPENDENCIES})
 
     setup_target_link_strategy(${EXECUTABLE_TARGET})
 
@@ -20,18 +20,18 @@ endfunction()
 
 function(setup_library)
     set(oneValueArgs TARGET)
-    set(multiValueArgs SOURCES INCLUDES LIBRARIES)
+    set(multiValueArgs SOURCES INCLUDES DEPENDENCIES)
 
-    cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(LIBRARY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    _setup_library_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
-    _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
-    _setup_target_libraries(${EXECUTABLE_TARGET} ${EXECUTABLE_LIBRARIES})
+    _setup_library_sources(${LIBRARY_TARGET} ${LIBRARY_SOURCES})
+    _setup_target_includes(${LIBRARY_TARGET} ${LIBRARY_INCLUDES})
+    _setup_target_dependencies(${LIBRARY_TARGET} ${LIBRARY_DEPENDENCIES})
 
-    setup_target_link_strategy(${EXECUTABLE_TARGET})
+    setup_target_link_strategy(${LIBRARY_TARGET})
 
-    setup_target_warnings(${EXECUTABLE_TARGET})
-    setup_target_for_sanitizer(${EXECUTABLE_TARGET})
+    setup_target_warnings(${LIBRARY_TARGET})
+    setup_target_for_sanitizer(${LIBRARY_TARGET})
 endfunction()
 
 macro(_setup_executable_sources target)
@@ -53,9 +53,18 @@ macro(_setup_target_includes target)
     )
 endmacro()
 
-macro(_setup_target_libraries target)
+macro(_setup_target_dependencies target)
+    set(multiValueArgs LINK_PATHS LIBRARIES)
+
+    cmake_parse_arguments(DEPENDENCY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    target_link_directories(${target}
+        PRIVATE
+            ${DEPENDENCY_LINK_PATHS}
+    )
+
     target_link_libraries(${target}
         PRIVATE
-            ${ARGN}
+            ${DEPENDENCY_LIBRARIES}
     )
 endmacro()
