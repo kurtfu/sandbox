bc16f683861652c9447b98b1a2f8d14df83ed971
Furkan Kurt <kurtfu@protonmail.com>
2024-07-17 21:45:31 +0300

    build: add custom vcpkg toolchains

---
+++ cmake/toolchains/x64-vcpkg.cmake
@@ -0,0 +1,11 @@
+if(WIN32)
+    if(${CMAKE_C_COMPILER} MATCHES "clang|gcc")
+        set(VCPKG_TARGET_TRIPLET x64-mingw-static)
+    endif()
+
+    if(${CMAKE_CXX_COMPILER} MATCHES "clang\\+\\+|g\\+\\+")
+        set(VCPKG_TARGET_TRIPLET x64-mingw-static)
+    endif()
+endif()
+
+include($ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake)
