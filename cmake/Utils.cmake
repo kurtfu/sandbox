e9828bed6a0e62bca29b2d7657f34a8e47e5149e
Furkan Kurt <kurtfu@protonmail.com>
2024-09-09 22:42:08 +0300

    build: use captured values in macros

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -2,54 +2,46 @@ include(Linker)
 include(Sanitizer)
 include(Warnings)
 
-function(setup_executable)
-    set(oneValueArgs TARGET)
+function(setup_executable target)
     set(multiValueArgs SOURCES INCLUDES DEPENDENCIES)
 
-    cmake_parse_arguments(EXECUTABLE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(TARGET "" "" "${multiValueArgs}" ${ARGN})
 
-    _setup_executable_sources(${EXECUTABLE_TARGET} ${EXECUTABLE_SOURCES})
-    _setup_target_includes(${EXECUTABLE_TARGET} ${EXECUTABLE_INCLUDES})
-    _setup_target_dependencies(${EXECUTABLE_TARGET} ${EXECUTABLE_DEPENDENCIES})
+    _setup_executable_sources(${target})
+    _setup_executable_includes(${target})
+    _setup_target_dependencies(${target})
 
-    setup_target_link_strategy(${EXECUTABLE_TARGET})
+    setup_target_link_strategy(${target})
 
-    setup_target_warnings(${EXECUTABLE_TARGET})
-    setup_target_for_sanitizer(${EXECUTABLE_TARGET})
+    setup_target_warnings(${target})
+    setup_target_for_sanitizer(${target})
 endfunction()
 
-function(setup_library)
-    set(oneValueArgs TARGET)
+function(setup_library target)
     set(multiValueArgs TYPE SOURCES INCLUDES DEPENDENCIES)
 
-    cmake_parse_arguments(LIBRARY "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(TARGET "" "" "${multiValueArgs}" ${ARGN})
 
-    _setup_library_sources(${LIBRARY_TARGET} ${LIBRARY_TYPE} ${LIBRARY_SOURCES})
-    _setup_library_includes(${LIBRARY_TARGET} ${LIBRARY_INCLUDES})
-    _setup_target_dependencies(${LIBRARY_TARGET} ${LIBRARY_DEPENDENCIES})
+    _setup_library_sources(${target})
+    _setup_library_includes(${target})
+    _setup_target_dependencies(${target})
 
-    setup_target_link_strategy(${LIBRARY_TARGET})
+    setup_target_link_strategy(${target})
 
-    setup_target_warnings(${LIBRARY_TARGET})
-    setup_target_for_sanitizer(${LIBRARY_TARGET})
+    setup_target_warnings(${target})
+    setup_target_for_sanitizer(${target})
 endfunction()
 
 macro(_setup_executable_sources target)
     add_executable(${target}
-        ${ARGN}
+        ${TARGET_SOURCES}
     )
 endmacro()
 
-macro(_setup_library_sources target type)
-    add_library(${target} ${type}
-        ${ARGN}
-    )
-endmacro()
-
-macro(_setup_target_includes target)
+macro(_setup_executable_includes target)
     set(multiValueArgs SYSTEM)
 
-    cmake_parse_arguments(INCLUDES "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(INCLUDES "" "" "${multiValueArgs}" ${TARGET_INCLUDES})
 
     target_include_directories(${target}
         PRIVATE
@@ -62,10 +54,16 @@ macro(_setup_target_includes target)
     )
 endmacro()
 
+macro(_setup_library_sources target type)
+    add_library(${target} ${type}
+        ${TARGET_SOURCES}
+    )
+endmacro()
+
 macro(_setup_library_includes target)
     set(multiValueArgs SYSTEM)
 
-    cmake_parse_arguments(INCLUDES "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    cmake_parse_arguments(INCLUDES "" "" "${multiValueArgs}" ${ARGN})
 
     target_include_directories(${target}
         PUBLIC
@@ -81,6 +79,6 @@ endmacro()
 macro(_setup_target_dependencies target)
     target_link_libraries(${target}
         PRIVATE
-            ${ARGN}
+            ${TARGET_DEPENDENCIES}
     )
 endmacro()
