234678b8f7598ac7df3679a1ce79fa283f79db8e
Furkan Kurt <kurtfu@protonmail.com>
2025-01-19 23:20:04 +0300

    refactor: add user input check for cli

--- tools/cli.py
+++ tools/cli.py
@@ -3,21 +3,32 @@ import readline
 import sys
 import threading
 
-lock = threading.Lock()
+__lock = threading.Lock()
+__is_waiting_input = False
 
 
 def println(message: str) -> None:
-    with lock:
+    with __lock:
         sys.stdout.write('\r\033[K')
         sys.stdout.write(f'{message}\n')
 
-        sys.stdout.write(f'> {readline.get_line_buffer()}')
-        sys.stdout.flush()
+        if __is_waiting_input:
+            sys.stdout.write(f'> {readline.get_line_buffer()}')
+            sys.stdout.flush()
 
 
 def input() -> str:
-    with lock:
+    global __is_waiting_input
+
+    with __lock:
+        __is_waiting_input = True
+
         sys.stdout.write('\r\033[K')
         sys.stdout.flush()
 
-    return builtins.input('> ')
+    prompt = builtins.input('> ')
+
+    with __lock:
+        __is_waiting_input = False
+
+    return prompt
